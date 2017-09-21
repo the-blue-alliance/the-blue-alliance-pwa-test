2484b25d97f691d54476af8042b35ecc4e4b31c0
Eugene Fang <fang.eugene@gmail.com>
2017-09-21 12:48:39 -0400

    Cleanup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,21 +3,11 @@ import { combineReducers } from 'redux-immutable'
 import appBar from './appBar'
 import models from './models'
 // import router from './router'
-// import database from './database'
-// import eventsByTeamByYear from './eventsByTeamByYear'
-// import infoByEvent from './infoByEvent'
-// import infoByTeam from './infoByTeam'
-// import teamsByEvent from './teamsByEvent'
 
 const reducer = combineReducers({
   appBar,
   models,
   // router,
-  // database,
-  // eventsByTeamByYear,
-  // infoByEvent,
-  // infoByTeam,
-  // teamsByEvent,
   // router: routerReducer,
 })
 
