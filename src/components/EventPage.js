9930c94b5c192fbe0c834723523fa9dc70029d7b
Eugene Fang <fang.eugene@gmail.com>
2017-09-24 23:33:53 -0400

    Responsive event page test

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import SwipeableViews from 'react-swipeable-views';
 import { withStyles } from 'material-ui/styles';
+import Hidden from 'material-ui/Hidden';
 import Tabs, { Tab } from 'material-ui/Tabs';
 import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import InfoOutlineIcon from 'material-ui-icons/InfoOutline';
 import EventIcon from 'material-ui-icons/Event';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
 
 import MatchList from './MatchList'
 import TeamsList from './TeamsList'
 
 import TBAPageContainer from '../containers/TBAPageContainer'
 
-const styles = {
-  slideContainer: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
+const styles = theme => ({
+  root: {
+    maxWidth: 1600,
+    margin: '0 auto',
+    padding: '0 48px',
   },
-}
+})
 
 class EventPage extends Component {
   constructor(props) {
@@ -62,52 +63,90 @@ class EventPage extends Component {
     }
 
     return (
-      <TBAPageContainer
-        title={name}
-        refreshFunction={this.refreshFunction}
-        tabs={
-          <Tabs
-            value={this.state.tabIdx}
-            onChange={this.tabHandleChange}
-            indicatorColor="white"
-            scrollable
-            scrollButtons="auto"
+      <div>
+        <Hidden smDown>
+          <TBAPageContainer
+            refreshFunction={this.refreshFunction}
           >
-            <Tab label={"Info"} />
-            <Tab label={"Teams"} />
-            <Tab label={"Matches"} />
-          </Tabs>
-        }
-      >
-        <SwipeableViews
-          containerStyle={styles.slideContainer}
-          index={this.state.tabIdx}
-          onChangeIndex={this.tabHandleChangeIndex}
-        >
-          <div>
-            <List>
-              <ListItem>
-                <ListItemIcon>
-                  <InfoOutlineIcon />
-                </ListItemIcon>
-                <ListItemText primary={name} />
-              </ListItem>
-            </List>
-            <Divider />
-            <List>
-              <ListItem>
-                <ListItemIcon>
-                  <EventIcon />
-                </ListItemIcon>
-                <ListItemText primary="Dates go here" />
-              </ListItem>
-            </List>
-          </div>
-          <TeamsList teams={teams} />
-          <MatchList matches={matches} />
-        </SwipeableViews>
-      </TBAPageContainer>
+            <div className={this.props.classes.root}>
+              <Grid container spacing={24}>
+                <Grid item xs={12}>
+                  <h1>{name}</h1>
+                </Grid>
+                <Grid item xs={12}>
+                  <Paper>
+                    <Tabs
+                      value={this.state.tabIdx}
+                      onChange={this.tabHandleChange}
+                      fullWidth
+                    >
+                      <Tab label="Matches" />
+                      <Tab label="Teams" />
+                    </Tabs>
+                  </Paper>
+                  {this.state.tabIdx === 0 && <div>Our old, familiar match table goes here!</div>}
+                  {this.state.tabIdx === 1 && <div>TEAMS!</div>}
+                </Grid>
+              </Grid>
+            </div>
+          </TBAPageContainer>
+        </Hidden>
+        <Hidden mdUp>
+          <TBAPageContainer
+            title={name}
+            refreshFunction={this.refreshFunction}
+            tabs={
+              <Tabs
+                value={this.state.tabIdx}
+                onChange={this.tabHandleChange}
+                indicatorColor="white"
+                scrollable
+                scrollButtons="auto"
+              >
+                <Tab label={"Info"} />
+                <Tab label={"Matches"} />
+                <Tab label={"Teams"} />
+              </Tabs>
+            }
+          >
+            <SwipeableViews
+              containerStyle={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+              }}
+              index={this.state.tabIdx}
+              onChangeIndex={this.tabHandleChangeIndex}
+            >
+              <div>
+                <List>
+                  <ListItem>
+                    <ListItemIcon>
+                      <InfoOutlineIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={name} />
+                  </ListItem>
+                </List>
+                <Divider />
+                <List>
+                  <ListItem>
+                    <ListItemIcon>
+                      <EventIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Dates go here" />
+                  </ListItem>
+                </List>
+              </div>
+              <MatchList matches={matches} />
+              <TeamsList teams={teams} />
+            </SwipeableViews>
+          </TBAPageContainer>
+        </Hidden>
+      </div>
     )
+
   }
 }
 
