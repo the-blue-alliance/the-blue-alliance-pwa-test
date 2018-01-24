2b5e4f13ffff9ece64f2795674dc3fcab81bc4b3
Eugene Fang <fang.eugene@gmail.com>
2018-01-23 19:45:16 -0500

    Lazy load event list card

--- src/components/EventListCard.js
+++ src/components/EventListCard.js
@@ -11,13 +11,24 @@ import IconButton from 'material-ui/IconButton'
 import Paper from 'material-ui/Paper'
 import Tooltip from 'material-ui/Tooltip'
 import Typography from 'material-ui/Typography'
+import VisibilitySensor from 'react-visibility-sensor'
 
 const styles = theme => ({
   eventListCard: {
     marginBottom: theme.spacing.unit*3,
   },
   eventListItem: {
+    height: 60,
     padding: `${theme.spacing.unit}px ${theme.spacing.unit*3}px`,
+  },
+  eventListItemInvisibleWithoutDivider: {
+    height: 60,
+  },
+  eventListItemInvisible: {
+    height: 61,
+  },
+  hiddenDivider: {
+    display: 'none',
   },
   verticalCenter: {
     height: '100%',
@@ -28,44 +39,75 @@ const styles = theme => ({
 })
 
 class EventListCard extends PureComponent {
+  state = {fastRender: true}
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ fastRender: false }), 0)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.events !== nextProps.events) {
+      this.setState({ fastRender: true })
+    }
+  }
+
+  componentDidUpdate() {
+    setTimeout(() => this.setState({ fastRender: false }), 0)
+  }
+
   render() {
     console.log("Render EventListCard")
 
-    const { classes } = this.props
+    const { classes, events } = this.props
 
     return (
       <Paper className={classes.eventListCard} elevation={4}>
-        {this.props.events.map((event, i) => {
+        {events.map((event, i) => {
           return (
-            <div key={event.key}>
-              <div className={classes.eventListItem}>
-                <Grid container spacing={24}>
-                  <Grid item xs={9}>
-                    <div className={classes.verticalCenter}>
-                      <Typography type='subheading' noWrap>
-                        <Link to={`/event/${event.key}`}>{event.name}</Link>
-                      </Typography>
-                      <Typography type='body1'>
-                        {event.getCityStateCountry()}
-                      </Typography>
-                    </div>
-                  </Grid>
-                  <Grid item xs={3}>
-                    <Typography type='body1' align='right' className={classes.verticalCenter}>
-                      {event.getDateString()}
-                    </Typography>
-                  </Grid>
-                  {/*<Grid item xs={1}>
-                    <Tooltip title='Event webcast is offline' placement='right'>
-                      <IconButton color='default' disabled>
-                        <Icon>videocam_off</Icon>
-                      </IconButton>
-                    </Tooltip>
-                  </Grid>*/}
-                </Grid>
-              </div>
-              {this.props.events.size !== i + 1 && <Divider />}
-            </div>
+            <VisibilitySensor
+              key={event.key}
+              partialVisibility
+            >
+              {({isVisible}) => {
+                if (isVisible || !this.state.fastRender) {
+                  return [
+                    <div className={classes.eventListItem} key='content'>
+                      <Grid container spacing={24}>
+                        <Grid item xs={9}>
+                          <div className={classes.verticalCenter}>
+                            <Typography type='subheading' noWrap>
+                              <Link to={`/event/${event.key}`}>{event.name}</Link>
+                            </Typography>
+                            <Typography type='body1'>
+                              {event.getCityStateCountry()}
+                            </Typography>
+                          </div>
+                        </Grid>
+                        <Grid item xs={3}>
+                          <Typography type='body1' align='right' className={classes.verticalCenter}>
+                            {event.getDateString()}
+                          </Typography>
+                        </Grid>
+                        {/*<Grid item xs={1}>
+                          <Tooltip title='Event webcast is offline' placement='right'>
+                            <IconButton color='default' disabled>
+                              <Icon>videocam_off</Icon>
+                            </IconButton>
+                          </Tooltip>
+                        </Grid>*/}
+                      </Grid>
+                    </div>,
+                    <Divider key='divider' className={events.size === i + 1 ? classes.hiddenDivider : null}/>,
+                  ]
+                } else {
+                  return (events.size === i + 1 ?
+                    <div className={classes.eventListItemInvisibleWithoutDivider} key='content' />
+                    :
+                    <div className={classes.eventListItemInvisible} key='content' />
+                  )
+                }
+              }}
+            </VisibilitySensor>
           )
         })}
       </Paper>
