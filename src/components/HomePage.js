6650d8499edad6431764e52de0eef4b8d46c7672
Eugene Fang <fang.eugene@gmail.com>
2018-01-24 17:18:02 -0500

    Update homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,17 @@
-import React, { PureComponent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { PureComponent } from 'react'
+import { withStyles } from 'material-ui/styles'
+import { Link } from 'react-router-dom'
+
+import Button from 'material-ui/Button'
 
 import TBAPageContainer from '../containers/TBAPageContainer'
 import ResponsiveLayout from './ResponsiveLayout'
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+})
 
 class Home extends PureComponent {
   constructor(props) {
@@ -12,19 +21,51 @@ class Home extends PureComponent {
   }
 
   render() {
-    console.log("Render Home");
+    console.log("Render Home")
+
+    const { classes } = this.props
+
     return (
       <TBAPageContainer
         history={this.props.history}
-        documentTitle='The Blue Alliance'
+        documentTitle=''
       >
         <ResponsiveLayout>
-          <h1>Home</h1>
-          <Link to="/team/254">254</Link>
+          <p>The Blue Alliance is the best way to scout, watch, and relive the <em>FIRST</em> Robotics Competition.</p>
+          <p>Welcome to a beta version of our new app!</p>
+
+          <p>Handy links for testing:</p>
+          <Button
+            color='default'
+            className={classes.button}
+            raised
+            component={Link}
+            to='/team/254/2017'
+          >
+            254 in 2017
+          </Button>
+          <Button
+            color='default'
+            className={classes.button}
+            raised
+            component={Link}
+            to='/event/2017casj'
+          >
+            2017casj
+          </Button>
+          <Button
+            color='default'
+            className={classes.button}
+            raised
+            component={Link}
+            to='/match/2017casj_f1m2'
+          >
+            2017casj_f1m2
+          </Button>
         </ResponsiveLayout>
       </TBAPageContainer>
     )
   }
 }
 
-export default Home;
+export default withStyles(styles)(Home)
