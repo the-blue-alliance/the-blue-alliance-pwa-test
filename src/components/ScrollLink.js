cdd15ad5084898d004b7359426d7f2e966e69938
Eugene Fang <fang.eugene@gmail.com>
2018-01-26 14:35:46 -0500

    Fix SweetScroll bug if scrollTop===0

--- src/components/ScrollLink.js
+++ src/components/ScrollLink.js
@@ -11,6 +11,18 @@ const styles = theme => ({
 class ScrollLink extends PureComponent {
   handleClick = (e) => {
     e.preventDefault()
+
+    // Weird bug breaks scrolling if scrollTop === 0
+    // https://github.com/tsuyoshiwada/sweet-scroll/issues/38
+    const els = document.getElementsByClassName(this.props.scrollEl.className)
+    let el = null
+    if (els) {
+      el = els[0]
+    }
+    if (el.scrollTop === 0) {
+      el.scrollTop = 1  // Set scrollTop = 1 to fix weird bug
+    }
+
     const scroller = new SweetScroll({
       duration: 250,
       easing: 'easeOutQuint',
