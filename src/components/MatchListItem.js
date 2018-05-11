e22e5e2971a518d342f1f679ff4e52a6c898544e
Eugene Fang <fang.eugene@gmail.com>
2018-05-11 18:48:47 -0400

    Use flexbox for match list item

--- src/components/MatchListItem.js
+++ src/components/MatchListItem.js
@@ -6,52 +6,56 @@ import classNames from 'classnames'
 import { withStyles } from 'material-ui/styles'
 
 // Components
-import Grid from 'material-ui/Grid'
 import { ListItem } from 'material-ui/List'
 import LinkContainer from 'react-router-bootstrap/lib/LinkContainer'
 
 const styles = theme => ({
   listItem: {
+    display: 'flex',
+    flexDirection: 'row',
     padding: theme.spacing.unit / 2,
   },
   matchName: {
-    display: 'flex',
-    flexDirection: 'column',
-    padding: theme.spacing.unit,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexGrow: 1,
     textAlign: 'center',
   },
-  table: {
-    padding: `${theme.spacing.unit}px 0`,
-    margin: 0,
-    width: '100%',
-    marginLeft: 'auto',
-    backgroundColor: '#ffffff',
-    borderCollapse: 'collapse',
-    fontSize: '16px',
-    '& tr': {
-      padding: `${theme.spacing.unit}px 0`,
+  match: {
+    flexGrow: 4,
+    textAlign: 'center',
+  },
+  alliance: {
+    display: 'flex',
+    flexDirection: 'row',
+    overflow: 'hidden',
+    borderRadius: theme.spacing.unit,
+    border: '2px solid transparent',
+  },
+  redAlliance: {
+    '& $team': {
+      backgroundColor: '#ffeeee',
     },
-    '& td': {
-      position: 'relative',
-      textAlign: 'center',
-      verticalAlign: 'middle !important',
-      padding: theme.spacing.unit,
+    '& $score': {
+      backgroundColor: '#ffdddd',
     },
   },
-  red: {
-    backgroundColor: '#ffeeee',
+  blueAlliance: {
+    '& $team': {
+      backgroundColor: '#eeeeff',
+    },
+    '& $score': {
+      backgroundColor: '#ddddff',
+    },
   },
-  blue: {
-    backgroundColor: '#eeeeff',
+  team: {
+    flexGrow: 3,
+    flexBasis: 0,
+    padding: theme.spacing.unit,
   },
-  redScore: {
-    backgroundColor: '#ffdddd',
-    fontWeight: 'bold',
-  },
-  blueScore: {
-    backgroundColor: '#ddddff',
+  score: {
+    position: 'relative',
+    flexGrow: 2,
+    flexBasis: 0,
+    padding: theme.spacing.unit,
     fontWeight: 'bold',
   },
   redWin: {
@@ -60,50 +64,91 @@ const styles = theme => ({
   blueWin: {
     border: '2px solid blue',
   },
+  rpDotA: {
+    position: 'absolute',
+    top: '3px',
+    left: '3px',
+    height: '4px',
+    width: '4px',
+  },
+  rpDotB: {
+    position: 'absolute',
+    top: '3px',
+    left: '10px',
+    height: '4px',
+    width: '4px',
+  },
 })
 
 class MatchListItem extends PureComponent {
   render() {
     const { classes, match } = this.props
+    let redScore = match.alliances.getIn(['red', 'score'])
+    let blueScore = match.alliances.getIn(['blue', 'score'])
+    if (redScore === -1) {
+      redScore = '?'
+    }
+    if (blueScore === -1) {
+      blueScore = '?'
+    }
     const redWin = match.winning_alliance === 'red'
     const blueWin = match.winning_alliance === 'blue'
+
     return (
       <ListItem button divider disableRipple className={classes.listItem}>
-        <Grid container className={classes.matchGrid}>
-          <LinkContainer to={{pathname: `/match/${match.key}`, state: {modal: true}}}>
-            <Grid item xs={3} className={classes.matchName}>
-              {match.getCompLevel()}
-              <br/>
-              {match.getSetMatch(true)}
-            </Grid>
-          </LinkContainer>
-          <Grid item xs={9}>
-            <table className={classes.table}>
-              <tbody>
-                <tr className={classNames({[classes.tr]: true, [classes.redWin]: redWin})}>
-                  {match.alliances.getIn(['red', 'team_keys']).map(teamKey => {
-                    return (
-                      <LinkContainer key={teamKey} to={{pathname: `/team/${teamKey.substring(3)}/${match.getYear()}`, hash: match.event_key.substring(4), state: {modal: true}}}>
-                        <td className={classes.red}>{teamKey.substring(3)}</td>
-                      </LinkContainer>
-                      )
-                  })}
-                  <td className={classes.redScore}>{match.alliances.getIn(['red', 'score'])}</td>
-                </tr>
-                <tr className={classNames({[classes.tr]: true, [classes.blueWin]: blueWin})}>
-                  {match.alliances.getIn(['blue', 'team_keys']).map(teamKey => {
-                    return (
-                      <LinkContainer key={teamKey} to={{pathname: `/team/${teamKey.substring(3)}/${match.getYear()}`, hash: match.event_key.substring(4), state: {modal: true}}}>
-                        <td className={classes.blue}>{teamKey.substring(3)}</td>
-                      </LinkContainer>
-                    )
-                  })}
-                  <td className={classes.blueScore}>{match.alliances.getIn(['blue', 'score'])}</td>
-                </tr>
-              </tbody>
-            </table>
-          </Grid>
-        </Grid>
+        <LinkContainer to={{pathname: `/match/${match.key}`, state: {modal: true}}}>
+          <div xs={3} className={classes.matchName}>
+            {match.getCompLevel()}
+            <br/>
+            {match.getSetMatch(true)}
+          </div>
+        </LinkContainer>
+        <div className={classes.match}>
+          <div className={classNames({[classes.alliance]: true, [classes.redAlliance]: true,  [classes.redWin]: redWin})}>
+            {match.alliances.getIn(['red', 'team_keys']).map(teamKey => {
+              return (
+                <LinkContainer key={teamKey} to={{pathname: `/team/${teamKey.substring(3)}/${match.getYear()}`, hash: match.event_key.substring(4), state: {modal: true}}}>
+                  <div className={classes.team}>{teamKey.substring(3)}</div>
+                </LinkContainer>
+                )
+            })}
+            <div className={classes.score}>
+              {match.rpEarnedA('red') &&
+                <svg className={classes.rpDotA}>
+                  <circle cx="2" cy="2" r="2"/>
+                </svg>
+              }
+              {match.rpEarnedB('red') &&
+                <svg className={classes.rpDotB}>
+                  <circle cx="2" cy="2" r="2"/>
+                </svg>
+              }
+              {redScore}
+            </div>
+          </div>
+          <div className={classNames({[classes.alliance]: true, [classes.blueAlliance]: true,  [classes.blueWin]: blueWin})}>
+            {match.alliances.getIn(['blue', 'team_keys']).map(teamKey => {
+              return (
+                <LinkContainer key={teamKey} to={{pathname: `/team/${teamKey.substring(3)}/${match.getYear()}`, hash: match.event_key.substring(4), state: {modal: true}}}>
+                  <div className={classes.team}>{teamKey.substring(3)}</div>
+                </LinkContainer>
+                )
+            })}
+            <div className={classes.score}>
+              {match.rpEarnedA('blue') &&
+                <svg className={classes.rpDotA}>
+                  <circle cx="2" cy="2" r="2"/>
+                </svg>
+              }
+              {match.rpEarnedB('blue') &&
+                <svg className={classes.rpDotB}>
+                  <circle cx="2" cy="2" r="2"/>
+                </svg>
+              }
+              {blueScore}
+            </div>
+          </div>
+        </div>
       </ListItem>
     )
   }
